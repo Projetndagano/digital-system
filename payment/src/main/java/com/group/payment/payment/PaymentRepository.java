fa15bbe02123da9eca6da0978911b8bfa83d19fe
PROJET NDAGANO <ndaganoprojet@gmail.com>
2026-04-07 19:27:57 +0300

    Fix: Move project to root for GitHub Actions

--- payment/src/main/java/com/group/payment/payment/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group.payment.payment;
-
-import com.group.payment.auth.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    List<Payment> findByUserOrderByCreatedAtDesc(User user);
-}
